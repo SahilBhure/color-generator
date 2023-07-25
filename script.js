95adf49d08c9a592dc703c2cac863ac934a804a3
SahilBhure <79324481+SahilBhure@users.noreply.github.com>
2023-07-25 15:01:01 +0530

    Removed third element script

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var css = document.querySelector('h3');
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
-var color3 = document.querySelector('.color3');
 var body = document.getElementById('Gradiant');
 
 function colorChange(){
@@ -9,10 +8,7 @@ function colorChange(){
     "linear-gradient(to right," 
     +color1.value
     +","
-    +color2.value
-    +","
-    +color3.value
-    ")";
+    +color2.value;
 
     css.textContent = body.style.background + ";";
 
@@ -20,5 +16,4 @@ function colorChange(){
 
 color1.addEventListener('input',colorChange);
 color2.addEventListener('input',colorChange);
-color3.addEventListener('input',colorChange);
 
